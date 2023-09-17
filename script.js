924640c3800b3770b99e5f61239b730268698fdb
sylsyd <129240987+sylsyd@users.noreply.github.com>
2023-09-17 15:35:55 +0800

    edt

--- script.js
+++ script.js
@@ -50,3 +50,45 @@ document.addEventListener("DOMContentLoaded", function() {
         entry.style.animationDelay = `${index * 0.2}s`;
     });
 });
+
+function showDetails(subject) {
+    alert(`This is a ${subject} class.`);
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const timetableContainer = document.querySelectorAll('.timetable-container');
+    
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('in-view');
+        }
+      });
+    }, { threshold: 0.1 });
+  
+    timetableContainer.forEach(container => {
+      observer.observe(container);
+    });
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const timetableDays = document.querySelectorAll('.day');
+    
+    let dayDelay = 0.5; // initial delay for the first day
+
+    timetableDays.forEach((day, dayIndex) => {
+        // Set the animation delay for each day
+        day.style.animationDelay = `${dayDelay}s`;
+        
+        const lessons = day.querySelectorAll('.lesson');
+        let lessonDelay = 0.2; // initial delay for the first lesson within a day
+
+        lessons.forEach((lesson, lessonIndex) => {
+            // Set the animation delay for each lesson, relative to its parent day
+            lesson.style.animationDelay = `${dayDelay + lessonDelay}s`;
+            lessonDelay += 0.2;
+        });
+
+        dayDelay += 0.5; // increment the delay for the next day
+    });
+});
